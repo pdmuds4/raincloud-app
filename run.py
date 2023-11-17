eca5200626fd4372bd602e4e7f07731b27216936
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-11-17 14:47:23 +0900

    Fix CORS origin URL in FastAPI middleware.

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from scripts.inference import predict
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://raincloud-app.vercel.app/"],
+    allow_origins=["https://raincloud-app.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
